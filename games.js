11fd9c97fa7423ba097a8982b92dd7e5094f178d
mystere974 <m.loghann@gmail.com>
2021-09-28 17:27:31 +0200

    first commit

--- games.js
+++ games.js
@@ -3,4 +3,14 @@ const mysql = require('./db-config')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  mysql.query('SELECT * FROM games', (err, result) => {
+    if (err) {
+      res.status(500).send('Error from Database')
+    } else {
+      res.status(200).json(result)
+    }
+  })
+})
+
 module.exports = router
--- players.js
+++ players.js
@@ -12,5 +12,21 @@ router.get('/', (req, res) => {
     }
   })
 })
+router.post('/', (req, res) => {
+  const bodyData = [
+    req.body.players_player,
+    req.body.players_weapon,
+    req.body.players_style
+  ]
+  const sql =
+    'INSERT INTO players (players_player, players_weapon, players_style) VALUES (?, ?, ?)'
+  mysql.query(sql, bodyData, (err, result) => {
+    if (err) {
+      res.status(500).send('Error from Database')
+    } else {
+      res.status(200).json(result)
+    }
+  })
+})
 
 module.exports = router
