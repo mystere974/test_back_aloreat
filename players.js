077cb213fb8578e14acdd4c10bcb7ed9c06f2d81
mystere974 <m.loghann@gmail.com>
2021-09-29 10:28:21 +0200

    add wednesday branch

--- players.js
+++ players.js
@@ -12,21 +12,36 @@ router.get('/', (req, res) => {
     }
   })
 })
+router.get('/:id', (req, res) => {
+  const playerId = req.params.id
+  mysql.query('SELECT * FROM players WHERE id=?', [playerId], (err, result) => {
+    if (err) {
+      res.status(500).send('error from database')
+    } else {
+      res.status(200).json(result)
+    }
+  })
+})
 router.post('/', (req, res) => {
-  const bodyData = [
-    req.body.players_player,
-    req.body.players_weapon,
-    req.body.players_style
-  ]
-  const sql =
-    'INSERT INTO players (players_player, players_weapon, players_style) VALUES (?, ?, ?)'
+  const bodyData = [req.body.player, req.body.weapon, req.body.style]
+  const sql = 'INSERT INTO players (player, weapon, style) VALUES (?, ?, ?)'
   mysql.query(sql, bodyData, (err, result) => {
     if (err) {
-      res.status(500).send('Error from Database')
+      res.status(500).send(err)
     } else {
       res.status(200).json(result)
     }
   })
 })
 
+router.delete('/:id', (req, res) => {
+  const playerId = req.params.id
+  mysql.query('DELETE FROM players WHERE id= ?', [playerId], (err, result) => {
+    if (err) {
+      res.status(500).status('Error deleted')
+    } else {
+      res.status(200).send('Player deleted')
+    }
+  })
+})
 module.exports = router
